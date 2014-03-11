8d70043b55c8c7e900d98d9594bda0093f7e118c
PramodHalapeti <Pramod.Halapeti@mckesson.com>
2014-03-11 16:52:16 -0700

    after chapter 1

--- OdeToFood/App_Start/RouteConfig.cs
+++ OdeToFood/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace OdeToFood
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Test",
+                url: "cuisine/{name}",
+                defaults: new { controller = "Cuisine", action = "Search", name = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class CuisineController : Controller
+    {
+        //
+        // GET: /Cuisine/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Search( string name)
+        {
+            var message = Server.HtmlEncode(name);
+
+            return Content(message);
+        }
+
+    }
+}
